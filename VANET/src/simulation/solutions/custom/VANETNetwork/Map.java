14b9b703052fd257644491d4eb29802afd932daa
Charles <Charles@172.27.20.2>
2013-01-17 10:54:57 +0100

    Correctifs: 	- Synchro code Charles/Vincent (Arc,Voiture,Map)

--- VANET/src/simulation/solutions/custom/VANETNetwork/Map.java
+++ VANET/src/simulation/solutions/custom/VANETNetwork/Map.java
@@ -34,53 +34,44 @@ public class Map {
 			 */
 			if (scanner.hasNextLine())
 			{
-				//Lecture du premier int correspondant au nombre de croisement
 				int nbCroisements = scanner.nextInt();
 				
 				for (int i = 1 ; i <= nbCroisements ; i++)
 				{
 					//recuperer croisement courant
-					Croisement croisCourant = new Croisement(scanner.nextInt(), scanner.nextInt(), scanner.nextLine());
+					Croisement croisCourant = new Croisement(scanner.nextInt(), scanner.nextInt(), scanner.nextLine().substring(1));
 					//l'ajouter à la liste
-					if (croisCourant != null)
-					{
-						//FIXME inutile, logiquement
-						System.out.println("i=" + i + ", Croisement courant =" + croisCourant.toString());
+					if (croisCourant != null)//FIXME inutile, logiquement ?
 						this.croisements.add(croisCourant);
-					}
 				}
-				int t = 0;	
 				//lecture des arcs
 				while (scanner.hasNext())
 				{
 					String d = scanner.next();
 					String f = scanner.next();
-					scanner.nextLine();
+					if (scanner.hasNextLine())
+						scanner.nextLine();
 					Iterator<Croisement> i = this.croisements.iterator();
 					Croisement deb = null, fin = null, croisCourant = null;
-			System.out.println("bou");
+
 					while (deb == null || fin == null)
-					{// On est sur de trouver les croisements qui correspondent (si le fichier est bien fait)						
-			t++;
-			System.out.println("t = "+t);
-						croisCourant = i.next();						
+					{// On est sur de trouver les croisements qui correspondent (si le fichier est bien fait)
+						croisCourant = i.next();
 						if (croisCourant.getName().equals(d))
 							deb = croisCourant;
 						if (croisCourant.getName().equals(f))
 							fin = croisCourant;
-			System.out.println("t = "+t);
 					}
 					this.arcs.add(new Arc(deb, fin));
 					// On ajoute également l'arc inverse dans tous les cas (à changer si l'on veut des rues à sens unique)
 					this.arcs.add(new Arc(fin, deb));
-			System.out.println("boui !");
 				}
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("Fichier Matrice introuvable");
 			e.printStackTrace();
 		}
-		 scanner.close();
+		scanner.close();
 	}
 	/**
 	 * @param name le nom du Croisement recherché
@@ -96,23 +87,21 @@ public class Map {
 			while (i.hasNext() && !cour.getName().equals(name))
 				cour = i.next();
 
-
 			if (!i.hasNext())// si je suis sur le dernier
 				cour = null;
 		}
-		
 		return cour;
 	}
 	
 	public String toString(){
-		String res = "";
+		String res = "Croisements :\n";
 		Iterator<Croisement>i = this.croisements.iterator();
 		while(i.hasNext())
-			res += i.next().toString();
-		
+			res += i.next().toString() + "\n";
+		res += "Arcs :\n";
 		Iterator<Arc>j = this.arcs.iterator();
 		while(j.hasNext())
-			res += j.next().toString();
+			res += j.next().toString() + "\n";
 		return res;
 	}
 }
--- VANET/src/simulation/solutions/custom/VANETNetwork/Voiture.java
+++ VANET/src/simulation/solutions/custom/VANETNetwork/Voiture.java
@@ -6,10 +6,20 @@ import simulation.multiagentSystem.MAS;
 import simulation.views.entity.imageInputBased.ImageFileBasedObjectView;
 
 import java.awt.Color;
+import java.io.CharArrayReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.io.BufferedReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.StringTokenizer;
 
+import javax.print.DocFlavor.READER;
 
 
 /**
@@ -20,16 +30,10 @@ import java.util.List;
 public class Voiture extends Agent implements ObjectAbleToSendMessageInterface 
 {	
 	//Référencement des ressources	
-	@SuppressWarnings("unused")
-	
-	private final static String CHEMIN_ACCES_MATRICE ="C:\\car.png";
-	
-	private final static String SPRITE_FILENAME_RIGHT = "\\VANET.Ressources\\Sprites\\carViewRight.bmp";//FIXME
-	private final static String SPRITE_FILENAME_LEFT = "\\VANET.Ressources\\Sprites\\carViewLeft.bmp";//FIXME
-	private final static String SPRITE_FILENAME_UP = "\\VANET.Ressources\\Sprites\\carViewUp.bmp";//FIXME
-	private final static String SPRITE_FILENAME_DOWN = "\\VANET.Ressources\\Sprites\\carViewDown.bmp";//FIXME
-	
+	private final static String CHEMIN_ACCES_MATRICE ="C:\\Users\\Wyvern\\Dropbox\\Projet VANET\\Code MASH\\Ressources de test";
+	private final static String SPRITE_FILENAME = "Chemin disque à remplir";//FIXME
 	private ImageFileBasedObjectView view;
+	private final static String SEPARATEUR_MATRICE_ARCS = "&&";
 	private Integer NOMBRE_VOITURE = 1;
 	private Integer NOMBRE_CROISEMENTS=9;
 	
@@ -52,76 +56,57 @@ public class Voiture extends Agent implements ObjectAbleToSendMessageInterface
 		this.setPosition(x, y);
 		this.setColor(c);
 		
-		//Création de la vue par défaut de la voiture avec estion des erreurs
-		try {
-			view = new ImageFileBasedObjectView(SPRITE_FILENAME_LEFT);
-		} catch (IOException InvalidInputFile) {
-			// TODO Auto-generated catch block
-			InvalidInputFile.printStackTrace();
-		}
-		
 		// l'endroit de départ ne figure pas dans le chemin à suivre (mais la voiture doit démarrer à coté de B !)
 		this.cheminASuivre.add(this.map.getCroisement("B"));
 		this.cheminASuivre.add(this.map.getCroisement("E"));
-		//Pour plus tard 
-		/*this.cheminASuivre.add(this.map.getCroisement("H"));
-		this.cheminASuivre.add(this.map.getCroisement("I"));*/
+		this.cheminASuivre.add(this.map.getCroisement("H"));
+		this.cheminASuivre.add(this.map.getCroisement("I"));
 	}
 
 	
 	public void run()
 	{
 		// wait a little amount of time to allow the construction of others agents 
-		try{Thread.sleep(500);}catch(Exception InvalidThreadException){}
+		try{Thread.sleep(500);}catch(Exception e){}
 		
 		while(!isKilling() && !isStopping())
 		{
 			Iterator<Croisement>iteratorCheminASuivre = this.cheminASuivre.iterator();
-				
-				this.allerA(iteratorCheminASuivre.next());
-				
-			//this.isKilling();
-		}		
+			Croisement prochaineDestination = iteratorCheminASuivre.next();
+			
+			
+		//	while(iteratorCheminASuivre.hasNext()){
+			
+				//aller jusqu'à la prochaine destination
+				while (! this.getPosition().equals(prochaineDestination.getPos()))
+				{// tant qu'on n'a pas atteint le prochain croisement
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					int deltaX = 0, deltaY = 0;
+					if (this.getPosition().x < prochaineDestination.getPos().x)
+						deltaX = 1;
+					else if (this.getPosition().x > prochaineDestination.getPos().x)
+						deltaX = -1;
+					if (this.getPosition().y < prochaineDestination.getPos().y)
+						deltaY = 1;
+					else if (this.getPosition().y > prochaineDestination.getPos().y)
+						deltaY = -1;
+					
+					this.setPosition(this.getPosition().x + deltaX, this.getPosition().y + deltaY);
+				}
+			//}
+			//puis faire le dernier mouvement ?
+			
+		}
+		
 	}
 
 	@Override
 	public void sendMessage(int receiver, String message) {
 		// TODO Auto-generated method stub
-		//
 		
-	}
-	
-	@SuppressWarnings("unused")
-	private void allerA(Croisement dest)
-	{
-		//aller jusqu'à la prochaine destination
-		
-		while (! this.getPosition().equals(dest.getPos()))
-		{
-			// tant qu'on n'a pas atteint le prochain croisement
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException InvalidThreadException) {
-				InvalidThreadException.printStackTrace();
-			}
-			
-			int deltaX = 0, deltaY = 0;
-			if (this.getPosition().x < dest.getPos().x)
-				deltaX = 1;
-			else if (this.getPosition().x > dest.getPos().x)
-				deltaX = -1;
-			if (this.getPosition().y < dest.getPos().y)
-				deltaY = 1;
-			else if (this.getPosition().y > dest.getPos().y)
-				deltaY = -1;
-			
-			this.setPosition(this.getPosition().x + deltaX, this.getPosition().y + deltaY);
-		}
-			
-	}
-	
-	@Override
-	public ImageFileBasedObjectView getView() {
-		return view;
 	}
 }
